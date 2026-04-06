94dccf2ef470777a926413d01196b37d6840b94e
unknown <apridika14@gmail.com>
2026-04-06 20:11:08 +0700

    done crud

--- includes/koneksi.php
+++ includes/koneksi.php
@@ -7,6 +7,8 @@ $db   = "tes_db";
 
 $conn = mysqli_connect("localhost", "root", "", "tes_db");
 
+date_default_timezone_set('Asia/Jakarta');
+
 if (!$conn) {
     die("Koneksi gagal: " . mysqli_connect_error());
 }
